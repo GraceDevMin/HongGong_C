554aacb90a0ae2fc0f3f4f4897574547c7772ea9
Eunhye <olivgrece@gmail.com>
2021-09-01 21:42:37 +0900

    ch03 210901_WED 학습

--- ch03/예제3-4.c
+++ ch03/예제3-4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-	unsigned int a;
-
-	a = 4294967295;	//큰 양수 저장
-	printf("%d\n", a);	//%d로 출력(부호있음으로 간주)
-	a = -1;	//음수 저장
-	printf("%u\n", a);	//%u로 출력(부호비트 고려X)
-	
-	/* unsigned 자료형은 항상 양수만 저장하고 %u로 출력!!*/
-}
+//int main(void) {
+//	unsigned int a;
+//
+//	a = 4294967295;	//큰 양수 저장
+//	printf("%d\n", a);	//%d로 출력(부호있음으로 간주)
+//	a = -1;	//음수 저장
+//	printf("%u\n", a);	//%u로 출력(부호비트 고려X)
+//	
+//	/* unsigned 자료형은 항상 양수만 저장하고 %u로 출력!!*/
+//}
---
+++ ch03/예제3-6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//int main(void) {
+//	char fruit[20] = "strawberry";	// char 배열 선언과 문자열 초기화
+//	//fruit = "banana";
+//	printf("딸기 : %s\n", fruit);	//배열명으로 저장된 문자열 출력
+//	printf("딸기잼 : %s %s\n", fruit, "jam");
+//}
