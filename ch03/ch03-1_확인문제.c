e6507d03ff55bb3c3dc17250a3907683013ce70a
Eunhye <olivgrece@gmail.com>
2021-09-02 21:19:46 +0900

    ch03 210902_THU 학습, ch03 ended

--- ch03/ch03-1_확인문제.c
+++ ch03/ch03-1_확인문제.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-	int kor = 70;
-	int eng = 80;
-	int mat = 90;
-
-	int tot = kor + eng + mat;
-	printf("국어 : %d, 영어 : %d, 수학 : %d\n", kor, eng, mat);
-	printf("총점 : %d\n", tot);
-
-}
+//int main(void) {
+//	int kor = 70;
+//	int eng = 80;
+//	int mat = 90;
+//
+//	int tot = kor + eng + mat;
+//	printf("국어 : %d, 영어 : %d, 수학 : %d\n", kor, eng, mat);
+//	printf("총점 : %d\n", tot);
+//}
---
+++ ch03/에제3-11.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main(void) {
+//	char grade;
+//	char name[20];
+//
+//	printf("학점 입력 : ");
+//	scanf("%c", &grade);
+//	printf("이름 입력 : ");
+//	scanf("%s", name);	//name 배열에 이름 문자열 입력, &를 사용하지 않는다
+//	printf("%s의 학점은 %c입니다.\n", name, grade);
+//
+//	return 0;
+//}
